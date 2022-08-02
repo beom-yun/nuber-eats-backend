47d78fb5370dc6faf9c65d3c43e0d8f5428db488
beom-yun <scds0326@naver.com>
2022-08-02 22:41:52 +0900

    Validating ArgsTypes

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,16 +1,22 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
+  @IsString()
   @Field(type => String)
+  @Length(5, 10)
   name: string;
 
+  @IsBoolean()
   @Field(type => Boolean)
   isVegan: boolean;
 
+  @IsString()
   @Field(type => String)
   address: string;
 
+  @IsString()
   @Field(type => String)
   ownersName: string;
 }
