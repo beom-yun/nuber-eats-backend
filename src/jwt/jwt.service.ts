18cd8d8a1e0fa5bbec1f47794e67c2a0991c691f
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-08-06 00:23:02 +0900

    JWT Module part Three

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from './jwt.constants';
 import { JwtModuleOptions } from './jwt.interfaces';
 
@@ -8,7 +9,7 @@ export class JwtService {
     @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
   ) {}
 
-  hello() {
-    console.log('hello');
+  sign(userId: number): string {
+    return jwt.sign({ id: userId }, this.options.privateKey);
   }
 }
