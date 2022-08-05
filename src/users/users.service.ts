0488862ba892568e8a21d16c79ba8e8896bb9e91
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-08-05 18:50:07 +0900

    Log In part Two

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -25,6 +26,26 @@ export class UsersService {
     } catch (e) {
       return { ok: false, error: "Couldn't create account" };
     }
-    // hash the password
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // check if the password is correct
+    // make a JWT and give it to the user
+    try {
+      const user = await this.users.findOne({ where: { email } });
+      if (!user) {
+        return { ok: false, error: 'User not found' };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return { ok: false, error: 'Wrong password' };
+      }
+      return { ok: true, token: 'lalala' };
+    } catch (error) {
+      return { ok: false, error };
+    }
   }
 }
